b35c003ea9a7e63a57ff85181b776c864d87ae8e
Evgeny Odegov <evg.odegov@gmail.com>
2024-07-18 18:37:30 +0700

    run_with_venv.sh: python -> python3

--- run_with_venv.sh
+++ run_with_venv.sh
@@ -4,4 +4,4 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 source ${SCRIPT_DIR}/.env/bin/activate
 
-python ${SCRIPT_DIR}/main.py $@
+python3 ${SCRIPT_DIR}/main.py $@
